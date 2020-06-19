dafd8076e1d22168409a9b56c5cb96a65382ecfc
keitasasaki4 <jcpmb136595@yahoo.co.jp>
2020-06-19 10:20:45 +0000

    modify

--- app/Micropost.php
+++ app/Micropost.php
@@ -13,18 +13,4 @@ class Micropost extends Model
         return $this->belongsTo(User::class);
     }
     
-    public function index()
-    {
-        $data = [];
-        if (\Auth::check()) {
-            $user = \Auth::user();
-            $microposts = $user->feed_microposts()->orderBy('created_at', 'desc')->paginate(10);
-
-            $data = [
-                'user' => $user,
-                'microposts' => $microposts,
-            ];
-        }
-        return view('welcome', $data);
-    }
 }
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,3 +1,5 @@
+@extends('layouts.app')
+
 @section('content')
     <div class="row">
         <aside class="col-sm-4">
