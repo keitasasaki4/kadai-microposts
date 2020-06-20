a393ab1c747c30d345ea63cf98eb4751b9551678
keitasasaki4 <jcpmb136595@yahoo.co.jp>
2020-06-20 11:17:18 +0000

    add favorite

--- app/User.php
+++ app/User.php
@@ -88,4 +88,32 @@ class User extends Authenticatable
         return Micropost::whereIn('user_id', $follow_user_ids);
     }
     
+    public function favorites()
+    {
+        return $this->belongsToMany(Micropost::class, 'favorites', 'user_id', 'micropost_id')->withTimestamps();
+    }
+    
+    public function favorite($micropostId)
+    {
+        $exist = $this->is_favorite($micropostId);
+    
+        if (!$exist) {
+            $this->favorites()->attach($micropostId);
+        }
+    }
+    
+    public function unfavorite($micropostId)
+    {
+        $exist = $this->is_favorite($micropostId);
+    
+        if ($exist) {
+            $this->favorites()->detach($micropostId);
+        }
+    }
+    
+    public function is_favorite($micropostId)
+    {
+        return $this->favorites()->where('micropost_id', $micropostId)->exists();
+    }
+
 }
